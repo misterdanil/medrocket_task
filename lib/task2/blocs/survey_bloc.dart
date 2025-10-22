f1f8f417caf51518d11e6eff5e2f62f72f0e40cf
misterdanil <jontio@mail.ru>
2025-10-22 23:42:53 +0300

    change variable names

--- lib/task2/blocs/survey_bloc.dart
+++ lib/task2/blocs/survey_bloc.dart
@@ -5,20 +5,20 @@ import 'package:medrocket_task/task2/model/survey_model.dart';
 import 'package:medrocket_task/task2/repository/survey_repository.dart';
 
 class SurveyBloc extends Bloc<SurveyEvent, SurveyState> {
-  final SurveyRepository _questionnaireRepository;
+  final SurveyRepository _surveyRepository;
 
-  SurveyBloc(this._questionnaireRepository)
+  SurveyBloc(this._surveyRepository)
     : super(SurveyNotFilledState()) {
     on<FetchSurveyEvent>((event, emit) async {
-      SurveyEntity? questionnaire = await _questionnaireRepository
+      SurveyEntity? survey = await _surveyRepository
           .getSurvey();
 
-      if (questionnaire == null || questionnaire.process == 0) {
+      if (survey == null || survey.process == 0) {
         emit(SurveyNotFilledState());
-      } else if (questionnaire.process > 0 && questionnaire.process < 100) {
-        emit(SurveyProcessingState(questionnaire));
+      } else if (survey.process > 0 && survey.process < 100) {
+        emit(SurveyProcessingState(survey));
       } else {
-        emit(SurveyFilledState(questionnaire));
+        emit(SurveyFilledState(survey));
       }
     });
   }
